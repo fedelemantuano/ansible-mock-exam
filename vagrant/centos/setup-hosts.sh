1b5eeae15b49a1c3030ffdbb424b5ad8f04f18cf
samperay <sunlnx@gmail.com>
2020-01-16 08:57:10 +0530

    vagrant infrastructure added

---
+++ vagrant/centos/setup-hosts.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+IFNAME=$1
+ADDRESS="$(ip -4 addr show $IFNAME | grep "inet" | head -1 |awk '{print $2}' | cut -d/ -f1)"
+sed -e "s/^.*${HOSTNAME}.*/${ADDRESS} ${HOSTNAME} ${HOSTNAME}.local/" -i /etc/hosts
+
+# Update /etc/hosts about other hosts
+cat >> /etc/hosts <<EOF
+192.168.120.11  ansible-1
+192.168.120.12  ansible-2
+192.168.120.13  ansible-3
+192.168.120.14  ansible-4
+EOF
+
+# install ansible package
+
+rpm --import /etc/pki/rpm-gpg/*
+sudo yum install ansible -y
+
